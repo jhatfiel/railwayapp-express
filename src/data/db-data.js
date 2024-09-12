c04efe7b60e4b6c3d38e2b4ea5bc171f9d5eb139
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-09-12 15:01:24 +0000

    Updating for 2024 (new api key)

--- src/data/db-data.js
+++ src/data/db-data.js
@@ -1,16 +1,24 @@
 import { readFile, writeFile } from 'fs/promises';
 import * as https from 'https';
 
-let GAMES = JSON.parse(await readFile('src/data/2023.json', 'utf8'));
-let GAMESP = JSON.parse(await readFile('src/data/2023P.json', 'utf8')); 
-let RANKINGS = JSON.parse(await readFile('src/data/2023_rankings.json', 'utf8'));
-let PREGAME = JSON.parse(await readFile('src/data/2023_pregame.json', 'utf8'));
-let PREGAMEP = JSON.parse(await readFile('src/data/2023P_pregame.json', 'utf8'));
-let lastUpdated = new Date();
-let latestRankingWeek = 1;
 let stale = 1000 * 60 * 60; // (1 hour)
 let CFBD_URL = 'https://api.collegefootballdata.com'
 let CFBD_OPTIONS = { headers: {accept: 'application/json', Authorization: `Bearer ${process.env['CFBD_TOKEN']}`}}
+let latestRankingWeek = 1;
+let lastUpdated = new Date();
+let GAMES;
+let GAMESP;
+let RANKINGS;
+let PREGAME;
+let PREGAMEP;
+
+await updateData(false);
+
+GAMES = JSON.parse(await readFile('src/data/2024.json', 'utf8'));
+GAMESP = JSON.parse(await readFile('src/data/2024P.json', 'utf8')); 
+RANKINGS = JSON.parse(await readFile('src/data/2024_rankings.json', 'utf8'));
+PREGAME = JSON.parse(await readFile('src/data/2024_pregame.json', 'utf8'));
+PREGAMEP = JSON.parse(await readFile('src/data/2024P_pregame.json', 'utf8'));
 
 function fixData() {
     let latestWeek = GAMES[GAMES.length-1].week;
@@ -30,65 +38,65 @@ function fixData() {
 
 fixData();
 
-async function updateData() {
-    if (new Date() - lastUpdated > stale) {
+async function updateData(forceUpdate = false) {
+    if (forceUpdate || new Date() - lastUpdated > stale) {
         console.log(`Updating data`);
         let GP = new Promise((resolve, reject) => {
-            https.get(`${CFBD_URL}/games?year=2023`, CFBD_OPTIONS, res => {
+            https.get(`${CFBD_URL}/games?year=2024`, CFBD_OPTIONS, res => {
                 let data = '';
                 res.on('data', chunk => { data += chunk });
                 res.on('close', () => {
                     GAMES = JSON.parse(data);
                     console.log(`Retrieved GAMES data`);
-                    writeFile('src/data/2023.json', data, 'utf8');
+                    writeFile('src/data/2024.json', data, 'utf8');
                     resolve(GAMES);
                 });
             }).on('error', err => { console.log(err.message); reject(err) });
         });
         let GPP = new Promise((resolve, reject) => {
-            https.get(`${CFBD_URL}/games?year=2023&seasonType=postseason`, CFBD_OPTIONS, res => {
+            https.get(`${CFBD_URL}/games?year=2024&seasonType=postseason`, CFBD_OPTIONS, res => {
                 let data = '';
                 res.on('data', chunk => { data += chunk });
                 res.on('close', () => {
                     GAMESP = JSON.parse(data);
                     console.log(`Retrieved GAMESP data`);
-                    writeFile('src/data/2023P.json', data, 'utf8');
+                    writeFile('src/data/2024P.json', data, 'utf8');
                     resolve(GAMESP);
                 });
             }).on('error', err => { console.log(err.message); reject(err) });
         });
         let RP = new Promise((resolve, reject) => {
-            https.get(`${CFBD_URL}/rankings?year=2023`, CFBD_OPTIONS, res => {
+            https.get(`${CFBD_URL}/rankings?year=2024`, CFBD_OPTIONS, res => {
                 let data = '';
                 res.on('data', chunk => { data += chunk });
                 res.on('close', () => {
                     RANKINGS = JSON.parse(data);
                     console.log(`Retrieved RANKINGS data`);
-                    writeFile('src/data/2023_rankings.json', data, 'utf8');
+                    writeFile('src/data/2024_rankings.json', data, 'utf8');
                     resolve(RANKINGS);
                 });
             }).on('error', err => { console.log(err.message); reject(err) });
         })
         let PP = new Promise((resolve, reject) => {
-            https.get(`${CFBD_URL}/metrics/wp/pregame?year=2023`, CFBD_OPTIONS, res => {
+            https.get(`${CFBD_URL}/metrics/wp/pregame?year=2024`, CFBD_OPTIONS, res => {
                 let data = '';
                 res.on('data', chunk => { data += chunk });
                 res.on('close', () => {
                     PREGAME = JSON.parse(data);
                     console.log(`Retrieved PREGAME data`);
-                    writeFile('src/data/2023_pregame.json', data, 'utf8');
+                    writeFile('src/data/2024_pregame.json', data, 'utf8');
                     resolve(PREGAME);
                 });
             }).on('error', err => { console.log(err.message); reject(err) });
         })
         let PPP = new Promise((resolve, reject) => {
-            https.get(`${CFBD_URL}/metrics/wp/pregame?year=2023&seasonType=postseason`, CFBD_OPTIONS, res => {
+            https.get(`${CFBD_URL}/metrics/wp/pregame?year=2024&seasonType=postseason`, CFBD_OPTIONS, res => {
                 let data = '';
                 res.on('data', chunk => { data += chunk });
                 res.on('close', () => {
                     PREGAMEP = JSON.parse(data);
                     console.log(`Retrieved PREGAMEP data`);
-                    writeFile('src/data/2023P_pregame.json', data, 'utf8');
+                    writeFile('src/data/2024P_pregame.json', data, 'utf8');
                     resolve(PREGAMEP);
                 });
             }).on('error', err => { console.log(err.message); reject(err) });
